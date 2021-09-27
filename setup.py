fe3c098b0a5a2822062cba88d6b52d20ffd295b8
Nicola Tonellotto <tonellotto@users.noreply.github.com>
2021-09-27 18:23:58 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+requirements = []
+with open('requirements.txt', 'rt') as f:
+    for req in f.read().splitlines():
+        if req.startswith('git+'):
+            pkg_name = req.split('/')[-1].replace('.git', '')
+            if "#egg=" in pkg_name:
+                pkg_name = pkg_name.split("#egg=")[1]
+            requirements.append(f'{pkg_name} @ {req}')
+        else:
+            requirements.append(req)
+#print(requirements)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyterrier-di",
+    version="0.0.1",
+    author="Nicola Tonellotto",
+    author_email='nicola.tonellotto{at}unipi.it',
+    description="PyTerrier components for DeepImpact",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
